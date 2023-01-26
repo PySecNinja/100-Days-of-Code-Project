904f5761eda9b53e70631d60853909021588ba52
Ahendrix9624 <33384698+Ahendrix9624@users.noreply.github.com>
2023-01-26 13:01:06 -0700

    Replaced 4 lines of code with 1 using list comprehension

--- day25/us-states-game/main.py
+++ day25/us-states-game/main.py
@@ -18,14 +18,17 @@ while len(guessed_states) < 50:
     guess = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
                             prompt="What's another state's name").title()
 
+
     if guess == "Exit":
-        missing_states = []
-        for state in list_of_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in list_of_states if state not in guessed_states]     # List comprehension got rid of lines 24 -27 with one line
+        # missing_states = []
+        # for state in list_of_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+    
     if guess in list_of_states:
         guessed_states.append(guess)
         t = turtle.Turtle()
